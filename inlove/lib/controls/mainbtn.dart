f29fcff09c34f9690997b6474479314280a2e172
Ronel Cruz <ronel.cruz.a8@gmail.com>
2022-05-19 05:59:40 +0200

    login screen

---
+++ inlove/lib/controls/mainbtn.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CustomButton extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 181,
+      height: 65,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                width: 181,
+                height: 53,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(32),
+                  color: Color(0xff1db9fc),
+                ),
+                padding: const EdgeInsets.only(
+                  left: 37,
+                  right: 69,
+                  top: 11,
+                  bottom: 9,
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Entrar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 105,
+            top: 0,
+            child: Container(
+                width: 66,
+                height: 65,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: SvgPicture.asset('assets/flecha.svg')),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- inlove/lib/screens/login.page.dart
+++ inlove/lib/screens/login.page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:inlove/controls/mainbtn.dart';
 
 class LoginPage extends StatefulWidget {
   static String routeName = "/LoginPage";
@@ -11,7 +12,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    final String usersAsset = 'assets/Users.svg';
+    final String usersAsset = 'assets/users.svg';
     final Widget user = SvgPicture.asset(
       usersAsset,
       // color: Colors.blue,
@@ -32,11 +33,104 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ],
             ),
-            Container(
-              color: Colors.red,
-              child: user,
-            )
+            // Container(
+            //   height: MediaQuery.of(context).size.height * .5,
+            //   width: MediaQuery.of(context).size.width * .7,
+            //   color: Colors.red,
+            //   child: user,
+            // ),
+            prebuiltPanel(),
+            CustomButton()
           ],
         ));
+  }
+
+  Widget prebuiltPanel() {
+    final String usersAsset = 'assets/users.svg';
+    final Widget user = SvgPicture.asset(
+      usersAsset,
+      // color: Colors.blue,
+    );
+    return Container(
+      width: 348,
+      height: 303,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width * .8,
+            height: MediaQuery.of(context).size.height * .7,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(26),
+              color: Color(0xff1b1b1b),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 36,
+                  top: 151,
+                  child: Container(
+                    width: 282,
+                    height: 47,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(13),
+                      color: Color(0xfc616161),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 36,
+                  top: 221,
+                  child: Container(
+                    width: 282,
+                    height: 47,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(13),
+                      color: Color(0xfc616161),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 87,
+                  top: 162,
+                  child: SizedBox(
+                    width: 231,
+                    child: Text(
+                      "Correo Electronico",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 21,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 139.99,
+                  top: 232,
+                  child: SizedBox(
+                    width: 51.26,
+                    child: Text(
+                      "Clave",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 21,
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    user,
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
